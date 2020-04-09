8491cc694d588725cff7f356d379017530e9f0b9
denon39 <dennonmeiklejohn@gmail.com>
2020-04-09 17:51:45 -0600

    Checkpoint 3 Unity
    Readme updates + Checkpoint 3 Unity Code

---
+++ HologramProject/Assets/OzMoveUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OzMoveUp : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y += .03f;
+            this.transform.position = position;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Movie-Gesture-Testing
 Latex Paper: https://www.overleaf.com/read/dzftdrwpmgqp  
-Checkpoint 1 Video Link: https://youtu.be/zUqzlOpw6GY
+Checkpoint 2 Video Link: https://youtu.be/zUqzlOpw6GY
+Checkpoint 3 Video Link: https://youtu.be/xFciJIbi_6w
 Unity Version: 2018.2.14f1
