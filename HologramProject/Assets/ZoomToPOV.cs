fa3a708ecede6da1f12b2cc8615fd4c29750879e
denon39 <dennonmeiklejohn@gmail.com>
2020-05-08 14:28:13 -0600

    Updated Scripts

--- HologramProject/Assets/ZoomToPOV.cs
+++ HologramProject/Assets/ZoomToPOV.cs
@@ -16,28 +16,30 @@ public class ZoomToPOV : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-			   Vector3 pos = GameObject.Find("MazeLowMan (1)").transform.position;
-				Quaternion rot = GameObject.Find("MazeLowMan (1)").transform.rotation;
+            Vector3 pos = GameObject.Find("MazeLowMan (1)").transform.position;
+            Quaternion rot = GameObject.Find("MazeLowMan (1)").transform.rotation;
             Vector3 position = this.transform.position;
-				Quaternion rotation = this.transform.rotation;
-				
-				pos.y += (float)0.45;
-								
-				Vector3 diff = pos - position;
-									
-				if (position != pos) {
-					position.x += diff.x/50;
-            	position.y += diff.y/50;
-					position.z += diff.z/50;
-				}
-				
-				if (rot != rotation) {
-					this.transform.rotation = Quaternion.RotateTowards(rotation, rot, (float)0.5);
-				}
-				
+            Quaternion rotation = this.transform.rotation;
+
+            pos.y += (float)0.45;
+
+            Vector3 diff = pos - position;
+
+            if (position != pos)
+            {
+                position.x += diff.x / 50;
+                position.y += diff.y / 50;
+                position.z += diff.z / 50;
+            }
+
+            if (rot != rotation)
+            {
+                this.transform.rotation = Quaternion.RotateTowards(rotation, rot, (float)0.5);
+            }
+
             this.transform.position = position;
         }
     }
-	 
-	 
+
+
 }
